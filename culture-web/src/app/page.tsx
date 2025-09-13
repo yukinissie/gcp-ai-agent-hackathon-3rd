f7eb1d1269fe75e3ad39685e260a02f7bd50a964
yukinissie <bigtree2490@outlook.jp>
2025-09-13 18:14:27 +0900

    feat: 文化関連コンテンツのためのメタデータとアイコンを追加し、文化画像を表示

--- culture-web/src/app/page.tsx
+++ culture-web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./index.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <h1 className={styles.title}>こんにちは!👋</h1>
       <p className={styles.description}>ここは始まりの村だよ！</p>
+      <Image src="/culture.png" alt="Culture" width={200} height={200} />
     </main>
   );
 }
