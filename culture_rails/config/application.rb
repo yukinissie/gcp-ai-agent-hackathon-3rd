d6fee2ff90ff79b10a6cc6bd704dd85292ddac11
YuSa0-6 <fuku.sugar@icloud.com>
2025-09-20 21:13:00 +0900

    railsをapiモードに変更する。

--- culture_rails/config/application.rb
+++ culture_rails/config/application.rb
@@ -11,6 +11,12 @@ module CultureRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.api_only = true
+
+    config.debug_exception_response_format = :default
+
+    config.debug_exception_response_format = :api
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
