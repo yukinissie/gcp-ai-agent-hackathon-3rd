b2ee0e646bb95f9b6b85ebb730d1958a0948da23
yukinissie <bigtree2490@outlook.jp>
2025-09-20 21:16:24 +0900

    Revert "feat: integrate Tailwind CSS and utility classes"
    This reverts commit 9058429e2e763171de86ca57fbc9b324b14208c9.

--- culture-web/src/app/page.tsx
+++ culture-web/src/app/page.tsx
@@ -2,21 +2,21 @@ import styles from "./index.module.css";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        gap: "1rem",
-        backgroundColor: "#f4fac0ff",
-      }}
-    >
-      <h1 className={styles.title}>Culture へようこそ!👋</h1>
-      <Image src="/culture.png" alt="Culture" width={150} height={150} />
-      <p className={styles.description}>ここは始まりの村だよ！</p>
-    </main>
-  );
+	return (
+		<main
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				height: "100vh",
+				gap: "1rem",
+				backgroundColor: "#f4fac0ff",
+			}}
+		>
+			<h1 className={styles.title}>Culture へようこそ!👋</h1>
+			<Image src="/culture.png" alt="Culture" width={150} height={150} />
+			<p className={styles.description}>ここは始まりの村だよ！</p>
+		</main>
+	);
 }
