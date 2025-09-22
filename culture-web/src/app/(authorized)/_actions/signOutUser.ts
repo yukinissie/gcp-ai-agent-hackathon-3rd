6376b172ebca8c9bbb7603cef4de9e956746bf07
yukinissie <bigtree2490@outlook.jp>
2025-09-22 11:26:12 +0900

    fix: signOutUserおよびsignInUser関数のエラーハンドリングを改善し、リダイレクトエラーを適切に処理

--- culture-web/src/app/(authorized)/_actions/signOutUser.ts
+++ culture-web/src/app/(authorized)/_actions/signOutUser.ts
@@ -1,11 +1,17 @@
 "use server";
 
 import { signOut } from "@/auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 export async function signOutUser() {
 	try {
-		await signOut();
+		await signOut({ redirectTo: "/" });
 	} catch (error) {
-		throw new Error("Sign-out failed");
+		if (isRedirectError(error)) {
+			throw error;
+		}
+		const errorMessage =
+			error instanceof Error ? error.message : "An unknown error occurred";
+		return { errorMessage };
 	}
 }
--- culture-web/src/app/(not-login)/_actions/signInUser.ts
+++ culture-web/src/app/(not-login)/_actions/signInUser.ts
@@ -1,11 +1,30 @@
 "use server";
 
 import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 export async function signInUser(formData: FormData) {
+	const credentials = Object.fromEntries(formData);
 	try {
-		await signIn("credentials", formData, { redirectTo: "/myfeed" });
+		await signIn("credentials", {
+			...Object.fromEntries(
+				Object.entries(credentials).map(([key, value]) => [
+					key,
+					value?.toString(),
+				]),
+			),
+			redirectTo: "/myfeed",
+		});
 	} catch (error) {
-		throw new Error("Sign-in failed: " + (error as Error).message);
+		if (error instanceof AuthError) {
+			throw new Error("Sign-in failed: " + error.message);
+		}
+		if (isRedirectError(error)) {
+			throw error;
+		}
+		const errorMessage =
+			error instanceof Error ? error.message : "An unknown error occurred";
+		return { errorMessage };
 	}
 }
