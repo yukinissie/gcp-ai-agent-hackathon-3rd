be86fd78196746de5d564cbdf02640c8063c8902
yukinissie <bigtree2490@outlook.jp>
2025-09-28 18:08:49 +0900

    fix: searched_atカラムを削除し、インデックスをuser_idとcreated_atに変更

--- culture_rails/db/migrate/20250928162700_create_tag_search_histories.rb
+++ culture_rails/db/migrate/20250928162700_create_tag_search_histories.rb
@@ -3,12 +3,11 @@ class CreateTagSearchHistories < ActiveRecord::Migration[7.1]
     create_table :tag_search_histories do |t|
       t.references :user, null: false, foreign_key: true
       t.integer :article_ids, null: false, default: [], array: true
-      t.datetime :searched_at, null: false, default: -> { "CURRENT_TIMESTAMP" }
       t.timestamps
     end
 
     add_index :tag_search_histories, :searched_at
-    add_index :tag_search_histories, [:user_id, :searched_at]
+    add_index :tag_search_histories, [:user_id, :created_at]
     add_index :tag_search_histories, :article_ids, using: :gin
   end
 end
