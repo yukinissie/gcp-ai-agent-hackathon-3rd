54206fcff7a023e68f23d431a68fcecbbb191b24
YuSa0-6 <fuku.sugar@icloud.com>
2025-09-28 18:25:42 +0900

    exec migration, create table.
    マイグレーションファイルを実行してDBを追加する。
    過去に吹き飛んだDBをRESETで追加し直す。

--- culture_rails/db/schema.rb
+++ culture_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_23_105709) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_28_162700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -93,118 +93,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_23_105709) do
     t.check_constraint "total_interactions >= 0", name: "check_total_interactions_positive"
   end
 
-  create_table "mastra_evals", id: false, force: :cascade do |t|
-    t.text "input", null: false
-    t.text "output", null: false
-    t.jsonb "result", null: false
-    t.text "agent_name", null: false
-    t.text "metric_name", null: false
-    t.text "instructions", null: false
-    t.jsonb "test_info"
-    t.text "global_run_id", null: false
-    t.text "run_id", null: false
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "createdAt", precision: nil
-    t.timestamptz "created_atZ", default: -> { "now()" }
-    t.timestamptz "createdAtZ", default: -> { "now()" }
-    t.index ["agent_name", "created_at"], name: "public_mastra_evals_agent_name_created_at_idx", order: { created_at: :desc }
-  end
-
-  create_table "mastra_messages", id: :text, force: :cascade do |t|
-    t.text "thread_id", null: false
-    t.text "content", null: false
-    t.text "role", null: false
-    t.text "type", null: false
-    t.datetime "createdAt", precision: nil, null: false
-    t.text "resourceId"
-    t.timestamptz "createdAtZ", default: -> { "now()" }
-    t.index ["thread_id", "createdAt"], name: "public_mastra_messages_thread_id_createdat_idx", order: { createdAt: :desc }
-  end
-
-  create_table "mastra_resources", id: :text, force: :cascade do |t|
-    t.text "workingMemory"
-    t.jsonb "metadata"
-    t.datetime "createdAt", precision: nil, null: false
-    t.datetime "updatedAt", precision: nil, null: false
-    t.timestamptz "createdAtZ", default: -> { "now()" }
-    t.timestamptz "updatedAtZ", default: -> { "now()" }
-  end
-
-  create_table "mastra_scorers", id: :text, force: :cascade do |t|
-    t.text "scorerId", null: false
-    t.text "traceId"
-    t.text "runId", null: false
-    t.jsonb "scorer", null: false
-    t.jsonb "preprocessStepResult"
-    t.jsonb "extractStepResult"
-    t.jsonb "analyzeStepResult"
-    t.float "score", null: false
-    t.text "reason"
-    t.jsonb "metadata"
-    t.text "preprocessPrompt"
-    t.text "extractPrompt"
-    t.text "generateScorePrompt"
-    t.text "generateReasonPrompt"
-    t.text "analyzePrompt"
-    t.text "reasonPrompt"
-    t.jsonb "input", null: false
-    t.jsonb "output", null: false
-    t.jsonb "additionalContext"
-    t.jsonb "runtimeContext"
-    t.text "entityType"
-    t.jsonb "entity"
-    t.text "entityId"
-    t.text "source", null: false
-    t.text "resourceId"
-    t.text "threadId"
-    t.datetime "createdAt", precision: nil, null: false
-    t.datetime "updatedAt", precision: nil, null: false
-    t.timestamptz "createdAtZ", default: -> { "now()" }
-    t.timestamptz "updatedAtZ", default: -> { "now()" }
-  end
-
-  create_table "mastra_threads", id: :text, force: :cascade do |t|
-    t.text "resourceId", null: false
-    t.text "title", null: false
-    t.text "metadata"
-    t.datetime "createdAt", precision: nil, null: false
-    t.datetime "updatedAt", precision: nil, null: false
-    t.timestamptz "createdAtZ", default: -> { "now()" }
-    t.timestamptz "updatedAtZ", default: -> { "now()" }
-    t.index ["resourceId", "createdAt"], name: "public_mastra_threads_resourceid_createdat_idx", order: { createdAt: :desc }
-  end
-
-  create_table "mastra_traces", id: :text, force: :cascade do |t|
-    t.text "parentSpanId"
-    t.text "name", null: false
-    t.text "traceId", null: false
-    t.text "scope", null: false
-    t.integer "kind", null: false
-    t.jsonb "attributes"
-    t.jsonb "status"
-    t.jsonb "events"
-    t.jsonb "links"
-    t.text "other"
-    t.bigint "startTime", null: false
-    t.bigint "endTime", null: false
-    t.datetime "createdAt", precision: nil, null: false
-    t.timestamptz "createdAtZ", default: -> { "now()" }
-    t.index ["name", "startTime"], name: "public_mastra_traces_name_starttime_idx", order: { startTime: :desc }
-  end
-
-  create_table "mastra_workflow_snapshot", id: false, force: :cascade do |t|
-    t.text "workflow_name", null: false
-    t.text "run_id", null: false
-    t.text "resourceId"
-    t.text "snapshot", null: false
-    t.datetime "createdAt", precision: nil, null: false
-    t.datetime "updatedAt", precision: nil, null: false
-    t.timestamptz "createdAtZ", default: -> { "now()" }
-    t.timestamptz "updatedAtZ", default: -> { "now()" }
-
-    t.unique_constraint ["workflow_name", "run_id"], name: "public_mastra_workflow_snapshot_workflow_name_run_id_key"
-  end
-
   create_table "pings", force: :cascade do |t|
     t.string "message", null: false
     t.datetime "created_at", null: false
@@ -218,6 +106,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_23_105709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "tag_search_histories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "article_ids", default: [], null: false, array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_ids"], name: "index_tag_search_histories_on_article_ids", using: :gin
+    t.index ["user_id", "created_at"], name: "index_tag_search_histories_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_tag_search_histories_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -253,5 +151,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_23_105709) do
   add_foreign_key "articles", "feeds"
   add_foreign_key "ingredients", "users"
   add_foreign_key "sessions", "users"
+  add_foreign_key "tag_search_histories", "users"
   add_foreign_key "user_credentials", "users"
 end
