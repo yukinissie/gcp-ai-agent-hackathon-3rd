942e569e278176494eb8f7f5bff6456a6f4dfe5e
yukinissie <bigtree2490@outlook.jp>
2025-10-05 18:33:06 +0900

    refactor: ArticleList コンポーネントのスタイルを簡素化し、不要なスタイルファイルを削除

--- culture-web/src/app/(authorized)/home/@articles/_styles/articleList.styles.ts
+++
@@ -1,5 +0,0 @@
-export const articleListStyles = {
-  emptyContainer: {
-    minHeight: '400px',
-  },
-} as const
--- culture-web/src/app/(authorized)/home/@articles/page.tsx
+++ culture-web/src/app/(authorized)/home/@articles/page.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Heading } from '@radix-ui/themes'
 import { ArticleList } from './_components/ArticleList'
 import { fetchArticles } from './_drivers/fetchArticles'
 import { fetchTagSearchHistoryArticles } from './_drivers/fetchTagSearchHistoryArticles'
@@ -6,12 +7,24 @@ export default async function ArticlesPage() {
   const articles = await fetchArticles()
   const tagSearchHistoryArticles = await fetchTagSearchHistoryArticles()
   return (
-    <ArticleList
-      articles={
-        tagSearchHistoryArticles.length > 0
-          ? tagSearchHistoryArticles
-          : articles
-      }
-    />
+    <Box>
+      <Flex direction="column" gap="6">
+        <Flex align="center" justify="between">
+          <Heading size="6" weight="bold">
+            記事一覧
+          </Heading>
+        </Flex>
+
+        <Flex direction="column" gap="3" width="100%">
+          <ArticleList
+            articles={
+              tagSearchHistoryArticles.length > 0
+                ? tagSearchHistoryArticles
+                : articles
+            }
+          />{' '}
+        </Flex>
+      </Flex>
+    </Box>
   )
 }
