3a508e445816ecdf0d5321d945a9790ac7b589fc
yukinissie <bigtree2490@outlook.jp>
2025-10-18 15:55:36 +0900

    feat: homeStylesのmainContainerのheightをminHeightに変更し、mainContentのoverflowプロパティを削除

--- culture-web/src/app/(authorized)/home/_styles/page.styles.ts
+++ culture-web/src/app/(authorized)/home/_styles/page.styles.ts
@@ -1,14 +1,11 @@
 export const homeStyles = {
   mainContainer: {
-    height: '100vh',
+    minHeight: '100vh',
   },
   mainContent: {
     flex: 1,
-    overflow: 'auto' as const,
     transition: 'margin-right 0.3s ease',
     marginRight: '0',
-    overflowX: 'auto' as const,
-    overflowY: 'auto' as const,
     transitionProperty: 'margin-right',
     transitionDuration: '0.3s',
     transitionTimingFunction: 'ease',
